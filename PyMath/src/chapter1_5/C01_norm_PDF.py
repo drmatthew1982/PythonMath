921b4c40b124359cdaebaff57711b819e8f311e0
Matthew Z <drmatthew1982@163.com>
2020-05-05 18:08:34 +0800

    For chapter1_5

---
+++ PyMath/src/chapter1_5/C01_norm_PDF.py
@@ -0,0 +1,21 @@
+'''
+Created on May 5, 2020
+
+@author: matthew.yiqing.zhu
+'''
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+seaborn.set()
+
+fig, ax = plt.subplots(1,1)
+norm_0 = norm(loc=0, scale=1)
+norm_1 = norm(loc=1, scale=2)
+
+x=np.linspace(-10,10,1000)
+ax.plot(x, norm_0.pdf(x), color="red", lw=5, alpha=0.6, label='loc=0, scale=1')
+ax.plot(x, norm_1.pdf(x), color="blue", lw=5, alpha=0.6, label='loc=1, scale=2')
+ax.legend(loc='best',frameon=False)
+
+plt.show()
